b7c9de1806b5cdb12741b04c02133f14142eb847
houra <houra.mehdipour@gmail.com>
2019-12-25 11:42:36 +0330

    add recycler

--- app/src/main/java/com/example/animalsproject/MainActivity.java
+++ app/src/main/java/com/example/animalsproject/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.animalsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recycler1 = findViewById(R.id.recycler1);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("All animals");
+        list.add("Birds");
+        list.add("Farm animals");
+        list.add("Wild animals");
+        list.add("Pet animals");
+        list.add("Mammals");
+        list.add("Reptiles and Amphibians");
+        list.add("Insects");
+        list.add("Land animals");
+        list.add("Water animals");
+
+
+
+
+
+        RecyclerOneAdapter adapter = new RecyclerOneAdapter(list);
+        recycler1.setAdapter(adapter);
+
+        recycler1.setLayoutManager(new LinearLayoutManager(MainActivity.this,RecyclerView.HORIZONTAL,false));
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/animalsproject/RecyclerOneAdapter.java
@@ -0,0 +1,48 @@
+package com.example.animalsproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerOneAdapter extends RecyclerView.Adapter<RecyclerOneAdapter.RecyclerOneViewHolder> {
+    ArrayList<String> myList;
+   public  RecyclerOneAdapter(ArrayList list){
+
+      myList = list;
+
+    }
+    @NonNull
+    @Override
+    public RecyclerOneViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler1_item,parent,false);
+       RecyclerOneViewHolder holder = new RecyclerOneViewHolder(v);
+       return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerOneViewHolder holder, int position) {
+
+
+             holder.txtAnimal.setText(myList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myList.size();
+    }
+
+    class RecyclerOneViewHolder extends  RecyclerView.ViewHolder{
+         TextView txtAnimal;
+        public RecyclerOneViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtAnimal =itemView.findViewById(R.id.txtAnimal);
+        }
+    }
+}
